e8425a86807b9da28a3d35cc778af2645c2d598a
sashrass <62172939+sashrass@users.noreply.github.com>
2020-03-18 12:34:25 +0300

    Update N.cs

--- Math_Module/Math_Modul/N.cs
+++ Math_Module/Math_Modul/N.cs
@@ -16,7 +16,8 @@ namespace Math_Module
 
         public N(List<string> s) // Александр Рассохин 9370
         {
-
+            for (int i=0; i < s.Count;i++)
+                znach.Add(Convert.ToUInt32(s[i]));
         }
 
         #endregion
